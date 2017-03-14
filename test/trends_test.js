acd1c242d9a52f20cfb167ef6fee83a173386297
Junpei Kawamoto <kawamoto.junpei@gmail.com>
2017-03-14 16:24:17 -0400

    trends returns a set of video IDs in the trend page.

---
+++ test/trends_test.js
@@ -0,0 +1,26 @@
+//
+// trends_test.js
+//
+// Copyright (c) 2016-2017 Junpei Kawamoto
+//
+// This software is released under the MIT License.
+//
+// http://opensource.org/licenses/mit-license.php
+//
+const {
+    assert
+} = require("chai");
+
+const trends = require("../lib/trends");
+
+describe("Trends function", function() {
+    this.timeout(60 * 1000);
+
+    it("Returns about fifty video IDs", () => {
+        return trends("JP").then((res) => {
+            assert.isAtLeast(res.length, 1, "There is at least one video IDs.");
+            assert.isAtMost(res.length, 50, "Trend video page has at most 50 videos.")
+        });
+    });
+
+});
